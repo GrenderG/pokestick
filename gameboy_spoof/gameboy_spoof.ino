9206b836b2e1631614c4ed6d9e60e21a967efd9f
Grant Haack <granthaack@hush.com>
2018-05-28 05:50:07 -0700

    reverted to Esteban Fuentealba's code cause mine was garbage and didn't work

--- gameboy_spoof/gameboy_spoof.ino
+++ gameboy_spoof/gameboy_spoof.ino
@@ -2,167 +2,145 @@
 #include "translators.h"
 #include "pokemon_generator.h"
 
-//Set up all the pins
-#define CLOCK_PIN 2
-#define MISO_PIN 3
-#define MOSI_PIN 4
-#define STATUS 13
+int volatile CLOCK_PIN = 2;
+int volatile MISO_PIN = 3;
+int volatile MOSI_PIN = 4;
+int volatile data = 0;
+int volatile val = 0;
+int ledStatus = 13;
 
-byte volatile in_buffer = 0x00;
-byte volatile out_buffer = 0x00;
+unsigned volatile long t_last_rec = 0;
+volatile byte out_buff = 0x00;
+volatile int clock_cycle = 0;
 
-int volatile clock_cycle = 0;
-int volatile trade_counter = 0;
-
+volatile int data_counter = 0;
 volatile connection_state_t connection_state = NOT_CONNECTED;
-volatile trade_center_state_t trade_center_state = INIT;
+volatile int trade_center_state = INIT;
+
+
 
 void setup() {
-  //Setting up what all the pins will be doing
   pinMode(MOSI_PIN, INPUT);
+  digitalWrite( MOSI_PIN, HIGH);  
   pinMode(MISO_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, INPUT);
-  pinMode(STATUS, OUTPUT);
-  digitalWrite(MOSI_PIN, HIGH);
+  pinMode(ledStatus, OUTPUT);
+  digitalWrite(ledStatus, LOW);
   digitalWrite(MISO_PIN, LOW);
+  pinMode(CLOCK_PIN, INPUT);
   digitalWrite(CLOCK_PIN, HIGH);
-  digitalWrite(STATUS, LOW);
-
-  //Attaching an interrupt to trigger whenever the clock pin reads high
-  attachInterrupt( digitalPinToInterrupt(CLOCK_PIN), clock_interrupt, RISING);
-  Serial.begin(115200);
+  Serial.begin(9600);
+  attachInterrupt( 0, clock_interrupt, RISING );
+  
 }
 
-void clock_interrupt(void)
-{
-  in_buffer <<= 1;
-  in_buffer |= (byte)digitalRead(MOSI_PIN);
-  //delayMicroseconds(22);
-  //Serial.println(in_buffer);
-  if(clock_cycle == 7)
-  {
-    Serial.print("r");
-    Serial.println(in_buffer, HEX);
-    out_buffer = handle_in_byte(in_buffer);
-    Serial.print("t");
-    Serial.println(out_buffer, HEX);
-    in_buffer = 0x00;
-    clock_cycle = 0;
+
+void clock_interrupt(void) {
+  byte in;
+  unsigned long t = 0;
+  if(t_last_rec > 0) {
+    if( micros() - t_last_rec > 120 ) {
+      clock_cycle = 0;
+      val = 0;
+      in = 0x00;
+    }
   }
-  if(out_buffer & 0x80)
-  {
-    digitalWrite(MISO_PIN, HIGH);
+  data = digitalRead(MOSI_PIN);
+  if(data == HIGH){
+    val |= ( 1 << (7-clock_cycle) );
+    in |= ( 1 << (7-clock_cycle) );
   }
-  else
-  {
-    digitalWrite(MISO_PIN, LOW);
+  if(clock_cycle == 7) {
+    out_buff = handle_in_byte((byte)val);
+    val = 0;
+    in = 0x00;
+    clock_cycle = -1;
   }
-  out_buffer <<= 1;
+  
   clock_cycle++;
+  t_last_rec = micros();
+  while( ((digitalRead(CLOCK_PIN) | CLOCK_PIN) & CLOCK_PIN)  == 0);
+  digitalWrite(MISO_PIN, out_buff & 0x80 ? MISO_PIN : 0);
+  out_buff = out_buff << 1;
 }
+byte handle_in_byte(byte in) {
+  byte send = 0x00;
+  switch(connection_state) {
+  case NOT_CONNECTED:
+    if(in == PKMN_MASTER)
+      send = PKMN_SLAVE;
+    else if(in == PKMN_BLANK)
+      send = PKMN_BLANK;
+    else if(in == PKMN_CONNECTED) {
+      send = PKMN_CONNECTED;
+      connection_state = CONNECTED;
+                        digitalWrite(ledStatus, HIGH);
+    }
+    break;
 
-byte handle_in_byte(byte in)
-{
-  switch(connection_state)
-  {
-    case NOT_CONNECTED:
-      if(in == PKMN_MASTER)
-      {
-        return PKMN_SLAVE;
-      }
-      else if(in == PKMN_BLANK)
-      {
-        return PKMN_BLANK;
-      }
-      else if(in == PKMN_CONNECTED)
-      {
-        connection_state = CONNECTED;
-        return PKMN_CONNECTED;
-        //digitalWrite(STATUS, HIGH);
-      }
-      break;
+  case CONNECTED:
+    if(in == PKMN_CONNECTED)
+      send = PKMN_CONNECTED;
+    else if(in == PKMN_TRADE_CENTER)
+      connection_state = TRADE_CENTER;
+    else if(in == PKMN_COLOSSEUM)
+      connection_state = COLOSSEUM;
+    else if(in == PKMN_BREAK_LINK || in == PKMN_MASTER) {
+      connection_state = NOT_CONNECTED;
+      send = PKMN_BREAK_LINK;
+                        digitalWrite(ledStatus, LOW);
+    } else {
+      send = in;
+    }
+    break;
 
-    case CONNECTED:
-      if(in == PKMN_CONNECTED)
-      {
-        return PKMN_CONNECTED;
+  case TRADE_CENTER:
+    if(trade_center_state == INIT && in == 0x00) {
+      if(data_counter++ == 5) {
+        trade_center_state = READY_TO_GO;
       }
-      else if(in == PKMN_TRADE_CENTER)
-      {
-        connection_state = TRADE_CENTER;
-        return PKMN_TRADE_CENTER;
+      send = in;
+    } else if(trade_center_state == READY_TO_GO && (in & 0xF0) == 0xF0) {
+      trade_center_state = SEEN_FIRST_WAIT;
+      send = in;
+    } else if(trade_center_state == SEEN_FIRST_WAIT && (in & 0xF0) != 0xF0) {
+      send = in;
+      data_counter = 0;
+      trade_center_state = SENDING_RANDOM_DATA;
+    } else if(trade_center_state == SENDING_RANDOM_DATA && (in & 0xF0) == 0xF0) {
+      if(data_counter++ == 5) {
+        trade_center_state = WAITING_TO_SEND_DATA;
       }
-      else if(in == PKMN_COLOSSEUM)
-      {
-        connection_state = COLOSSEUM;
-        return COLOSSEUM;
+      send = in;
+    } else if(trade_center_state == WAITING_TO_SEND_DATA && (in & 0xF0) != 0xF0) {
+      data_counter = 0;
+      send = PKMN_DATA[data_counter++];
+      trade_center_state = SENDING_DATA;
+    } else if(trade_center_state == SENDING_DATA) {
+      send = PKMN_DATA[data_counter++];
+      if(data_counter == 415) {
+        trade_center_state = DATA_SENT;
       }
-      else if((in == PKMN_BREAK_LINK) || (in == PKMN_MASTER))
-      {
-        connection_state = NOT_CONNECTED;
-        return PKMN_BREAK_LINK;
-        //digitalWrite(STATUS, LOW);
-      }
-      else
-      {
-        return in;
-      }
-      break;
-      /*
-    case TRADE_CENTER:
-      if(trade_center_state == INIT && in == 0x00) 
-      {
-        if(trade_counter++ == 5)
-        {
-          trade_center_state = READY_TO_GO;
-        }
-        send_byte = in;
-      }
-      else if(trade_center_state == READY_TO_GO && (in & 0xF0) == 0xF0)
-      {
-        trade_center_state = SEEN_FIRST_WAIT;
-        send_byte = in;
-      }
-      else if(trade_center_state == SEEN_FIRST_WAIT && (in & 0xF0) != 0xF0)
-      {
-        send_byte = in;
-        trade_counter = 0;
-        trade_center_state = SENDING_RANDOM_DATA;
-      } 
-      else if(trade_center_state == SENDING_RANDOM_DATA && (in & 0xF0) == 0xF0)
-      {
-        if(trade_counter++ == 5)
-        {
-          trade_center_state = WAITING_TO_SEND_DATA;
-        }
-        send_byte = in;
-      }
-      else if(trade_center_state == WAITING_TO_SEND_DATA && (in & 0xF0) != 0xF0)
-      {
-        trade_counter = 0;
-        send_byte = POKEMON_DATA[trade_counter++];
-        trade_center_state = SENDING_DATA;
-      }
-      else if(trade_center_state == SENDING_DATA)
-      {
-        send_byte = POKEMON_DATA[trade_counter++];
-        if(trade_counter == 415)
-        {
-          trade_center_state = DATA_SENT;
-        }
-      }
-      else
-      {
-        send_byte = in;
-      }
-      break;
-*/  
-    default:
-      return in;
+    } else {
+      send = in;
+    }
+    break;
+
+  case COLOSSEUM:
+    send = in;
+    break;
+
+  default:
+    send = in;
+    break;
   }
-  return in;
+
+  return send;
 }
-void loop() 
-{
-}
+
+void loop() { 
+
+ }
+
+
+
 
