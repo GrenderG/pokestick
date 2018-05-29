02f1aee367aaef7bcfe4210a16ba8a361ca86f71
Grant Haack <granthaack@hush.com>
2018-05-29 01:46:00 -0700

    Finished the gen_pokemon function

--- gameboy_spoof/pokemon_generator.c
+++ gameboy_spoof/pokemon_generator.c
@@ -1,6 +1,150 @@
 #include "pokemon_generator.h"
 #include "translators.h"
 #include "dictionaries.h"
+
+typedef unsigned char byte;
+
+uint8_t* gen_pokemon(struct pokemon* in_poke)
+{
+  //The final buffer to hold the whole pokemon
+  uint8_t* data; 
+  data = (uint8_t*)malloc(44);
+
+  uint32_t tmp = 0;
+  uint8_t byte_1 = 0;
+  uint8_t byte_2 = 0;
+  uint8_t byte_3 = 0;
+    
+  data[0x00] = in_poke->index;
+  
+  tmp = in_poke->hp;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF); 
+  data[0x01] = byte_1;
+  data[0x02] = byte_2;
+  
+  data[0x03] = in_poke->pc_lvl;
+  data[0x04] = in_poke->status_cond;
+  data[0x05] = in_poke->type1;
+  data[0x06] = in_poke->type2;
+  data[0x07] = in_poke->catch_or_held_item;
+  data[0x08] = in_poke->mov1;
+  data[0x09] = in_poke->mov2;
+  data[0x0A] = in_poke->mov3;
+  data[0x0B] = in_poke->mov4;
+
+  tmp = in_poke->trainer_id;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x0C] = byte_1;
+  data[0x0D] = byte_2;
+
+  tmp = in_poke->exp_pts;
+  byte_3 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x0E] = byte_1;
+  data[0x0F] = byte_2;
+  data[0x10] = byte_3;
+
+  tmp = in_poke->hp_ev;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x11] = byte_1;
+  data[0x12] = byte_2;
+
+  tmp = in_poke->attack_ev;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x13] = byte_1;
+  data[0x14] = byte_2;
+
+  tmp = in_poke->defense_ev;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x15] = byte_1;
+  data[0x16] = byte_2;
+
+  tmp = in_poke->speed_ev;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x17] = byte_1;
+  data[0x18] = byte_2;
+
+  tmp = in_poke->special_ev;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x19] = byte_1;
+  data[0x1A] = byte_2;
+
+  tmp = in_poke->iv_data;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x1B] = byte_1;
+  data[0x1C] = byte_2;
+  
+  data[0x1D] = in_poke->mov1_pp;
+  data[0x1E] = in_poke->mov2_pp;
+  data[0x1F] = in_poke->mov3_pp;
+  data[0x20] = in_poke->mov4_pp;
+  data[0x21] = in_poke->lvl;
+
+  tmp = in_poke->max_hp;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x22] = byte_1;
+  data[0x23] = byte_2;
+
+  tmp = in_poke->attack_stat;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x24] = byte_1;
+  data[0x25] = byte_2;
+
+  tmp = in_poke->defense_stat;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x26] = byte_1;
+  data[0x27] = byte_2;
+
+  tmp = in_poke->speed_stat;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x28] = byte_1;
+  data[0x29] = byte_2;
+
+  tmp = in_poke->special_stat;
+  byte_2 = (tmp & 0x000000FF); 
+  tmp >>= 8;
+  byte_1 = (tmp & 0x000000FF);
+  data[0x2A] = byte_1;
+  data[0x2B] = byte_2;
+
+  return data;
+}
+
+uint8_t* gen_trade(struct trade* in_trade)
+{
+  //The final buffer to hold the whole trade
+  uint8_t* data; 
+  data = (uint8_t*)malloc(415);
+  
+}
+
 struct pokemon pk_test = {
   //Index Number
   PKI_MRMIME,
@@ -29,17 +173,17 @@ struct pokemon pk_test = {
   //EXP
   100,
   //HP EV Data
-  100000,
+  0xFFFF,
   //Attack EV Data
-  100000,
+  0xFFFF,
   //Defense EV Data
-  100000,
+  0xFFFF,
   //Speed EV Data
-  100000,
+  0xFFFF,
   //Special EV Data
-  100000,
+  0xFFFF,
   //IV Data
-  100000,
+  0xFFFF,
   //Move 1 PP
   0,
   //Move 2 PP
@@ -49,15 +193,15 @@ struct pokemon pk_test = {
   //Move 4 PP
   0,
   //Level
-  50,
+  5,
   //Max HP
   40,
   //Attack
-  10000,
+  0xFFFF,
   //Defense
-  10000,
+  0xFFFF,
   //Speed
-  10000,
+  0xFFFF,
   //Special
-  10000
+  0xFFFF
 };
--- gameboy_spoof/pokemon_generator.h
+++ gameboy_spoof/pokemon_generator.h
@@ -1,40 +1,78 @@
 #ifndef POKEMON_GENERATOR_H
 #define POKEMON_GENERATOR_H
 
+#include <stdint.h>
+
 typedef unsigned char byte;
 
 struct pokemon
 {
-  byte ind_num;
-  unsigned int hp;
-  byte pc_lvl;
-  byte stat_cond;
-  byte type1;
-  byte type2;
-  byte catch_or_held_itm;
-  byte mov_1;
-  byte mov_2;
-  byte mov_3;
-  byte mov_4;
-  unsigned int train_id;
-  unsigned long ex_pts;
-  unsigned int hp_ev;
-  unsigned int att_ev;
-  unsigned int def_ev;
-  unsigned int spd_ev;
-  unsigned int spc_ev;
-  unsigned int iv_dat;
-  byte mov_1_pp;
-  byte mov_2_pp;
-  byte mov_3_pp;
-  byte mov_4_pp;
-  byte lvl;
-  unsigned int max_hp;
-  unsigned int att;
-  unsigned int def;
-  unsigned int spd;
-  unsigned int spc;
+  uint8_t index;
+  uint16_t hp;
+  uint8_t pc_lvl;
+  uint8_t status_cond;
+  uint8_t type1;
+  uint8_t type2;
+  uint8_t catch_or_held_item;
+  uint8_t mov1;
+  uint8_t mov2;
+  uint8_t mov3;
+  uint8_t mov4;
+  uint16_t trainer_id;
+  uint32_t exp_pts; //Only the first 3 bytes of this are valid, but there's no uint24_t
+  uint16_t hp_ev;
+  uint16_t attack_ev;
+  uint16_t defense_ev;
+  uint16_t speed_ev;
+  uint16_t special_ev;
+  uint16_t iv_data;
+  uint8_t mov1_pp;
+  uint8_t mov2_pp;
+  uint8_t mov3_pp;
+  uint8_t mov4_pp;
+  uint8_t lvl;
+  uint16_t max_hp;
+  uint16_t attack_stat;
+  uint16_t defense_stat;
+  uint16_t speed_stat;
+  uint16_t special_stat;
 };
 
+struct trade
+{
+  char trader_name[10];
+  uint8_t num_of_pokemon;
+  uint8_t pki1;
+  uint8_t pki2;
+  uint8_t pki3;
+  uint8_t pki4;
+  uint8_t pki5;
+  uint8_t pki6;
+  uint8_t pki7;
+  struct pokemon poke1;
+  /*
+   * Storing all 7 pokemon will quickly fill up the Arduino's RAM so I'm not doing it yet. Just going to fill the buffer with 0s
+  struct pokemon poke2;
+  struct pokemon poke3;
+  struct pokemon poke4;
+  struct pokemon poke5;
+  struct pokemon poke6;
+  struct pokemon poke7;
+  */
+  char poke_name1[10];
+  char poke_name2[10];
+  char poke_name3[10];
+  char poke_name4[10];
+  char poke_name5[10];
+  char poke_name6[10];
+  char poke_name7[10];
+  char trainer_name1[10];
+  char trainer_name2[10];
+  char trainer_name3[10];
+  char trainer_name4[10];
+  char trainer_name5[10];
+  char trainer_name6[10];
+  char trainer_name7[10];
+};
 #endif
 
